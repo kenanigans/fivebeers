d5c88a2f99a696b24b823b302b3951c46857a581
chrismccoy <fris@fris.net>
2015-06-10 06:55:17 -0400

    removed github widgets

--- functions.php
+++ functions.php
@@ -60,9 +60,4 @@ include(get_stylesheet_directory() . '/inc/layout.php');
 */
 include(get_stylesheet_directory() . '/video-player/video-player.php');
 
-/**
- * GitHub Widgets and Shortcodes
- *
-*/
-include(get_stylesheet_directory() . '/inc/github.php');
 
